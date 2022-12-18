cab8c1c5876a77cf71de580d90e8aaeb163e2e6b
imshubhu <techshubhu@gmail.com>
2022-12-18 18:11:41 +0530

    first commit

---
+++ components/About.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import AboutImg from '../public/assets/about.jpg';
+
+const About = () => {
+  return (
+    <div id='about' className='w-full md:h-screen p-2 flex items-center py-16'>
+      <div className='max-w-[1240px] m-auto md:grid grid-cols-3 gap-8'>
+        <div className='col-span-2'>
+          <p className='uppercase text-xl tracking-widest text-[#5651e5]'>
+            About
+          </p>
+          <h2 className='py-4'>Who I Am</h2>
+          <p className='py-2 text-gray-600'>
+            I'm a full stack developer with a passion for creating innovative solutions. I understand the complexities of developing applications, from the back-end to the front-end. My experience includes working on various technologies, such as JavaScript, Node.js, React.js, Angular, Nextjs, and HTML/CSS. I'm also skilled in docker, server configuration, databases, and problem solving. I strive to always stay up-to-date with the latest trends and technologies. In my free time, I'm passionate about coding projects and tinkering with new programming languages. I believe that being a great developer is not using one
+              specific language, but choosing the best tool for the job.
+          </p>
+          <p className='py-2 text-gray-600'>
+            I started web developement in 2019 managing multiple e-commerce
+            websites on Angular and React Framework. I have experience working directly with clients and taking
+            mock wireframes all the way to deployed applications. Also I have worked with Web3 technologies like metamask, solidity and ipfs system.
+          </p>
+          <Link href='/#projects'>
+            <p className='py-2 text-gray-600 underline cursor-pointer'>
+              Check out some of my latest projects.
+            </p>
+          </Link>
+        </div>
+        <div className='w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300'>
+          <Image src={AboutImg} className='rounded-xl' alt='/' />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ pages/resume.jsx
@@ -0,0 +1,183 @@
+import React from 'react';
+import Head from 'next/head';
+import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
+
+const resume = () => {
+  return (
+    <>
+      <Head>
+        <title>Shubham | Resume</title>
+        <meta
+          name='description'
+          content='I’m a full-stack web developer specializing in building (and occasionally designing) exceptional digital experiences.'
+        />
+        <link rel='icon' href='/fav.png' />
+      </Head>
+
+      <div className='max-w-[940px] mx-auto p-2 pt-[120px]'>
+        <h2 className='text-center'>Resume</h2>
+        <div className='bg-[#d0d4d6] my-4 p-4 w-full flex justify-between items-center'>
+          <h2 className='text-center'>Shubham Lohar</h2>
+          <div className='flex'>
+            <a
+              href='https://www.linkedin.com/in/shubham-lohar-467531161/'
+              target='_blank'
+              rel='noreferrer'
+            >
+              <FaLinkedinIn size={20} style={{ marginRight: '1rem' }} />
+            </a>
+            <a
+              href='https://github.com/imshubhu'
+              target='_blank'
+              rel='noreferrer'
+            >
+              <FaGithub size={20} style={{ marginRight: '1rem' }} />
+            </a>
+          </div>
+        </div>
+        <div className='text-center py-4 text-xl font-bold uppercase tracking-wider'>
+          <div className='hidden sm:block'>
+            <p>
+              Proven Leadership <span className='px-1'>|</span> Web Development{' '}
+              <span className='px-1'>|</span> Complex Problem Solving
+            </p>
+          </div>
+          <div className='block sm:hidden'>
+            <p>Proven Leadership</p>
+            <p className='py-2'>Web Development</p>
+            <p>Complex Problem Solving</p>
+          </div>
+        </div>
+        <p>
+          I'm an enthusiastic, responsible, and hard-working person. Being worked on different projects
+          helped me to adapt to the changes quickly and made me a mature team worker. I am able to work
+          well both in team environment as well as using my own initiative. I am able to work well underline
+          pressure and adhere to strict deadlines.
+        </p>
+
+        {/* Skills */}
+        <div className='text-center py-4'>
+          <h5 className='text-center underline text-[18px] py-2'>Skills</h5>
+          <p className='py-2'>
+            <span className='font-bold'>Technical Skills</span>
+            <span className='px-2'>|</span>Front-End Web Developer
+            <span className='px-2'>|</span>Back-End Web Developer
+            <span className='px-2'>|</span> HTML
+            <span className='px-2'>|</span>CSS
+            <span className='px-2'>|</span>Javascript
+            <span className='px-2'>|</span>Angular
+            <span className='px-2'>|</span>React
+            <span className='px-2'>|</span>Next JS
+            <span className='px-2'>|</span>SQL
+            <span className='px-2'>|</span>NoSQL
+            <span className='px-2'>|</span>Redux
+            <span className='px-2'>|</span>Tailwind
+            <span className='px-2'>|</span>Docker
+            <span className='px-2'>|</span> Firebase
+            <span className='px-2'>|</span> RESTAPI
+          </p>
+          <p className='py-2'>
+            <span className='font-bold'>Amazon Web Services</span>
+            <span className='px-2 font-bold'>|</span>
+            <span className='font-bold'>Digital Ocean</span>
+          </p>
+        </div>
+
+        <h5 className='text-center underline text-[18px] py-4'>
+          Professional Experience
+        </h5>
+        {/* Experience */}
+        <div className='py-6'>
+          <p className='italic'>
+            <span className='font-bold italic'>
+            Adore India
+            </span>
+            <span className='px-2'>|</span>Virtual Intern
+          </p>
+          <p className='py-1 italic'>Back End Web Developer (JUL 2020 - OCT 2020)</p>
+          <ul className='list-disc list-outside px-7 py-1 leading-relaxed'>
+            <li>
+              Worked on RTC technology using agora.
+            </li>
+            <li>
+              Troubleshoot issues and concerns, configure operation and analytic
+              plugins, and design changes as needed.
+            </li>
+            <li>
+              Worked directly with a client base to understand and
+              implement multiple technologies and programs.
+            </li>
+          </ul>
+        </div>
+
+
+        {/* Personal Experience */}
+        <div className='py-6'>
+          <p className='italic'>
+            <span className='font-bold italic'>
+              DayDream Software LLP
+            </span>
+            <span className='px-2'>|</span>Surat, Gujarat
+          </p>
+          <p className='py-1 italic'>Full-Stack Developer (OCT 2020 - Current)</p>
+          <ul className='list-disc list-outside px-7 py-1 leading-relaxed'>
+          <li>
+              Worked directly with a client base to understand and
+              implement multiple technologies and programs.
+            </li>
+            <li>
+              Troubleshoot issue and solve bugs of project.
+            </li>
+            <li>
+              Configuration of server.
+            </li>
+          </ul>
+        </div>
+
+        {/*  */}
+        {/* <h5 className='text-center underline text-[18px] py-4'>
+          Other Professional Experience
+        </h5> */}
+      
+        {/* Experience */}
+        {/* <div className='py-6'>
+          <p className='italic'>
+            <span className='font-bold'>CITY OF SHERMAN, TX</span>
+            <span className='px-2'>|</span>Sherman, TX
+          </p>
+          <p className='py-1 italic'>Paramedic / Firefighter (2010 – 2019)</p>
+          <ul className='list-disc list-outside px-7 py-1 leading-relaxed'>
+            <li>
+              Ensured proper implementation of firefighting techniques and
+              practices including controlling and extinguishing fires, operating
+              hose lines, and fire ground operations, ventilation, and search
+              and rescue.
+            </li>
+            <li>
+              Oversaw comprehensive understanding, operation, and maintenance of
+              fire engines, ladder trucks, technical rescue apparatus, and
+              response trailers.
+            </li>
+            <li>
+              Performed ALS techniques as needed within emergency situations
+              including the use of an esophageal or dual lumen airway device and
+              oral intubation using laryngoscopy.
+            </li>
+            <li>
+              Collaborated with a talented and high performing team of
+              firefighters in high pressure and stressful situations as needed.
+            </li>
+            <li>
+              Utilized proven and demonstrated knowledge of multiple
+              certifications including Pediatric Advanced Life Support,
+              Prehospital Trauma Life Support, and Advanced Cardiac Life
+              Support, among others.
+            </li>
+          </ul>
+        </div> */}
+      </div> 
+    </>
+  );
+};
+
+export default resume;
